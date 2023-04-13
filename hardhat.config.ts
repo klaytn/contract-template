c98b5432b22ae4a4cd1510269bf86f9be6a59710
Chihyun Song <ian0371@gmail.com>
2023-04-13 17:25:06 +0900

    Add hardhat compiler optimization

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,14 @@ import * as dotenv from "dotenv";
 const defaultKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+    ],
+  },
   networks: {
     baobab: {
       url: process.env.BAOBAB_URL || "https://archive-en.baobab.klaytn.net",
